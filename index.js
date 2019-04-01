ba76cb7b526921eaea459f5925fa1721e01c7ff0
Mauricio Feldman-Abe <6901736+mauabe@users.noreply.github.com>
2019-04-01 02:30:42 -0700

    added selector

---
+++ index.js
@@ -0,0 +1,13 @@
+var button = document.getElementsByTagName('button')[0]
+
+button.addEventListener('click', function() {
+  console.log('clicking')
+})
+
+// button.addEventListener('mouseenter', function() {
+//   console.log('entering')
+// })
+
+// button.addEventListener('mouseleave', function(){
+//   console.log('leaving')
+// });
